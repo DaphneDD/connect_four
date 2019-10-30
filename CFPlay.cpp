b7a76e19e3ec784d4d74f31639bf903028b0b8fa
DaphneDD <dongxq12@gmail.com>
2019-10-30 15:38:57 -0700

    ch

--- CFPlay.cpp
+++ CFPlay.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "CFGame.hpp"
 
@@ -11,9 +12,26 @@ using std::endl;
 using std::cin;
 using std::ofstream;
 using std::ifstream;
+using std::vector;
 
 
 int main()
 {
+	ifstream fin("results.txt");
+	vector<Player*> players;
+	string name;
+	int score;
+	if (fin.is_open())
+	{
+		while (getline(fin, name))
+		{
+			fin >> score;
+			players.push_back(new Player(name, score));
+		}
+		fin.close();
+	}
+	
+	
+	
 	return 0;
 }
